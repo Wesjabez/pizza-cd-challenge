79f23ca85677e1d769cd8c83dbb0d8e2ca1cf77a
Wesley <wesochienga@gmail.com>
2023-10-02 13:02:41 +0300

    fifth commit

--- app/models/pizza.py
+++ app/models/pizza.py
@@ -7,7 +7,7 @@ from .config import db
 class Pizza(db.Model):
     
     id = db.Column(db.Integer ,primary_key = True)
-    name = db.Column(db.String(255), nullable = False)
-    ingredients = db.Column(db.String(255), nullable = False)
+    name = db.Column(db.String(200), nullable = False)
+    ingredients = db.Column(db.String(200), nullable = False)
 
     restaurant_pizzas = db.relationship("RestaurantPizza", back_populates='pizza')
--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -5,7 +5,7 @@ from .config import db
 
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(255), nullable = False)
-    address = db.Column(db.String(255), nullable = False)
+    name = db.Column(db.String(200), nullable = False)
+    address = db.Column(db.String(200), nullable = False)
    
     restaurant_pizzas = db.relationship("RestaurantPizza",  back_populates="restaurant")
